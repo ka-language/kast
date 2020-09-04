e8849cc2c660dee83e945ea56f77bf6056096347
Ankizle <ankizleyt@gmail.com>
2020-09-03 23:26:21 -0400

    made the cwd happen in the ps1

--- oat.ps1
+++ oat.ps1
@@ -1,3 +1,9 @@
 #!/usr/bin/env pwsh
 $cwd = ("$PWD").replace("\", "/")
-& "$PSScriptRoot\oat_start.exe" $args -cwd="$cwd"
+
+if ($args[0] -ne "build" -and $args[0] -ne "run") {
+    $args = ,"run" + $args
+}
+
+chdir "$cwd"
+& "$PSScriptRoot\oat_start.exe" $args
--- oat_start.go
+++ oat_start.go
@@ -12,9 +12,7 @@ import (
 	"github.com/omm-lang/omm/lang/types"
 )
 
-var cwd = flag.String("cwd", "", "Set the current working directory (automatically placed by the shell/pwsh script)")
 var output = flag.String("out", "", "Set output file")
-var prec = flag.Uint64("prec", 20, "Set the precision of an Omm instance when running an oat file")
 
 func init() {
 	flag.Usage = suite.Usagef("Oat")
@@ -43,12 +41,17 @@ func main() {
 		*output = strings.TrimSuffix(filename, filepath.Ext(filename)) + ".oat" //remove the .omm and replace with .oat
 	}
 	cli_params.Output = *output
-	cli_params.Prec = *prec
 
 	if opt == "build" {
 		//if they want to build an oat
 		oat.Compile(cli_params)
 	} else if opt == "run" {
+
+		if *output == "" {
+			fmt.Println("Error, cannot use -out while running an oat file")
+			os.Exit(1)
+		}
+
 		//if they want to run an oat
 		oat.Run(cli_params)
 	} else {
